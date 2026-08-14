d4a38b15410ef8d0a3496ebea7130ef27a63b55c
Fw1zzy <emmanuelpascua.ph@gmail.com>
2026-08-15 05:10:31 +0800

    hotfix:seo

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -15,7 +15,7 @@ const Themes = () => {
   return (
     <div className='ml-5'>
       <label id="theme-toggle-button">
-        <input type="checkbox" id="toggle" onChange={toggleTheme} />
+        <input type="checkbox" id="toggle" onChange={toggleTheme} aria-label="Toggle theme" />
         <svg viewBox="0 0 69.667 44" xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
           <g transform="translate(3.5 3.5)" data-name="Component 15 – 1" id="Component_15_1">
             
